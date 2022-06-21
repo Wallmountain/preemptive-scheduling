efbdf2c41fbbf25c6665f2b397dca5c5abed3d4d
Wallmountain <Benny5090596@gmail.comls>
2022-06-22 00:38:10 +0800

    signal

--- jmp.c
+++ jmp.c
@@ -3,7 +3,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <ucontext.h>
 #include "list.h"
+
+static int preempt_count = 0;
+static void preempt_disable(void) {
+    preempt_count++;
+}
+static void preempt_enable(void) {
+    preempt_count--;
+}
+
+static void local_irq_save(sigset_t *sig_set) {
+    sigset_t block_set;
+    sigfillset(&block_set);
+    sigdelset(&block_set, SIGINT);
+    sigprocmask(SIG_BLOCK, &block_set, sig_set);
+}
+
+static void local_irq_restore(sigset_t *sig_set) {
+    sigprocmask(SIG_SETMASK, sig_set, NULL);
+}
+
+
+#define task_printf(...)     \
+    ({                       \
+        preempt_disable();   \
+        printf(__VA_ARGS__); \
+        preempt_enable();    \
+    })
+
+static void timer_create(unsigned int usecs) {
+    ualarm(usecs, usecs);
+}
+
+static void timer_cancel(void) {
+    ualarm(0, 0);
+}
+
+static void re_schedule(void) {
+    sigset_t set;
+    local_irq_save(&set);
+
+    printf("signal\n");
+
+    local_irq_restore(&set);
+}
+
+static void timer_handler(int signo, siginfo_t *info, ucontext_t *ctx) {
+    if(preempt_count)
+        return;
+
+    re_schedule();
+}
+
+static void timer_init(void) {
+    struct sigaction sa = {
+        .__sigaction_handler = (void (*)(int)) timer_handler,
+        .sa_flags = SA_SIGINFO};
+    sigfillset(&sa.sa_mask);
+    sigaction(SIGALRM, &sa, NULL);
+}
 
 struct task {
     jmp_buf env;
@@ -104,7 +165,7 @@ void task1(void *arg)
 
     if (setjmp(env) == 0) {
         task_add(&tasklist, env);
-        longjmp(sched, 1);;
+        longjmp(sched, 1);
     }
 
     static int i = 0;
@@ -123,11 +184,20 @@ void task1(void *arg)
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 int main(void)
 {
+    timer_init();
+
     void (*registered_task[])(void *) = {task0, task1};
     tasks = registered_task;
     ntasks = ARRAY_SIZE(registered_task);
 
+    preempt_disable();
+    timer_create(1000); /* 10 ms */
+    preempt_enable();
+
     schedule();
+
+    preempt_enable();
+    timer_cancel();
 
     return 0;
 }
